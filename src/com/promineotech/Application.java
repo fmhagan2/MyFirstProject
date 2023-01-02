c6a3be2a1d0c87c7325393b269422d164995641a
fmhagan2 <fmhagan2@gmail.com>
2023-01-02 16:55:19 -0600

    https://github.com/fmhagan2/MyFirstProject

--- src/com/promineotech/Application.java
+++ src/com/promineotech/Application.java
@@ -5,7 +5,8 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("My first Java Program!");
-	System.out.println("git repo video fourth commit");
+	System.out.println("git repo video commit");
+	System.out.println("git repo test");
 	}
 
 }
