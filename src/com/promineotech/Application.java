c51d12ac0de1bf3c216989746d306e7a4ce66b6e
fmhagan2 <fmhagan2@gmail.com>
2023-01-02 17:03:05 -0600

    My First Commit

--- src/com/promineotech/Application.java
+++ src/com/promineotech/Application.java
@@ -5,8 +5,6 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("My first Java Program!");
-	System.out.println("git repo video commit");
-	System.out.println("git repo test");
 	}
 
 }
