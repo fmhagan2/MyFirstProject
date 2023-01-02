7bbafa7a37b900c12e52b03435ff613ce4ded456
fmhagan2 <fmhagan2@gmail.com>
2023-01-02 16:12:43 -0600

    First ACTUAL commit

---
+++ src/com/promineotech/Application.java
@@ -0,0 +1,11 @@
+package com.promineotech;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("My first Java Program!");
+	System.out.println("git repo video");
+	}
+
+}
