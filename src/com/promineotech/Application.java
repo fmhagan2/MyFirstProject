9c8d77775bd90b205ceb036bb47a28ff44f3adc8
fmhagan2 <fmhagan2@gmail.com>
2023-01-02 17:06:14 -0600

    Second commit

--- src/com/promineotech/Application.java
+++ src/com/promineotech/Application.java
@@ -5,6 +5,7 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 	System.out.println("My first Java Program!");
+	System.out.println("git repo video");
 	}
 
 }
